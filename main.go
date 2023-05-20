23bf6bfe82d5e8e17784234ffd39e747e5f00579
Jonathan Luiz <luiz_jal@yahoo.com.br>
2023-05-20 16:41:21 +0200

    Update Github

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
+		w.Write([]byte("Hello Jonathan"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
