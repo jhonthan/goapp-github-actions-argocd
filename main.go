039b3f236355f60696d4ae5001a93ee3ca728739
Jonathan Luiz <luiz_jal@yahoo.com.br>
2023-05-21 08:16:24 +0200

    new deploy stage

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello Jonathan !! New Port"))
+		w.Write([]byte("Hello Jonathan !! New"))
 	})
 	http.ListenAndServe(":9090", nil)
 }
